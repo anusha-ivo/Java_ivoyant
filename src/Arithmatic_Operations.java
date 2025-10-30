21217594ba15330f500e3bc09eade2b4b7fa750d
anusha-82 <anusha525@gmail.com>
2025-10-30 17:45:01 +0530

    first commit

---
+++ src/Arithmatic_Operations.java
@@ -0,0 +1,19 @@
+public class Operations {
+    public static void main(String args[]){
+        int a=10;
+        int b=12;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a%b;
+        int g=a/b;
+        System.out.println(" A value  is : "+a);
+        System.out.println(b);
+        System.out.println(" addition is : "+c);
+        System.out.println(" subtraction is : "+d);
+        System.out.println(" multiplication is : "+e);
+        System.out.println(" division  is : "+f);
+        System.out.println(" quotient is : "+g);
+
+    }
+}
---
+++ src/Labeled_loop.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class Labeled_loop {
+}
